3ae5feb0a69e915c473e4995655d3ae4802b17ca
Avi Fisher <avifisher82@gmail.com>
2025-11-30 14:49:37 +0200

    first commit

---
+++ week1/test.js
@@ -0,0 +1,56 @@
+// 1
+
+// let a = 5
+// console.log(a);
+
+// a = 8
+// console.log(a)
+
+
+// 2 
+
+
+// let b = 12
+// console.log(b);
+
+// b += 3
+// console.log(b);
+
+
+// 3
+
+// const c = 4
+// console.log(c);
+
+
+
+// 4
+// const d = 10
+// d = 20
+// console.log(d);
+
+
+
+// 5
+
+// var e = 2
+// e = 7
+// console.log(7);
+
+
+// 6
+// var f = 100
+// var f = 200
+// console.log(f);
+
+
+// 7
+
+let g = 3
+console.log(g);
+
+g = g * 2
+console.log(g);
+
+
+
